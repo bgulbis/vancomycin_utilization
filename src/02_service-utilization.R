866733e5c375829a9a572f25d1fc3d30bd8e46cc
bgulbis <brian.gulbis@gmail.com>
2018-05-01 10:21:32 -0500

    fy18 vanc consults

--- src/02_service-utilization.R
+++ src/02_service-utilization.R
@@ -5,13 +5,17 @@ library(edwr)
 dir_raw <- "data/raw/consults"
 
 consults <- read_data(dir_raw, "consults", FALSE) %>%
-    rename(millennium.id = `Encounter Identifier`,
-           order.datetime = `Date and Time - Order Start`,
-           order = `Mnemonic (Primary Generic) FILTER ON`,
-           order.location = `Nurse Unit (Order)`) %>%
+    rename(
+        millennium.id = `Encounter Identifier`,
+        order.datetime = `Date and Time - Order Start`,
+        order = `Mnemonic (Primary Generic) FILTER ON`,
+        order.location = `Nurse Unit (Order)`
+    ) %>%
     format_dates("order.datetime") %>%
-    filter(order.datetime >= mdy("10/1/2017", tz = "US/Central"),
-           order.datetime < mdy("1/1/2018", tz = "US/Central")) %>%
+    filter(
+        order.datetime >= mdy("7/1/2017", tz = "US/Central"),
+        order.datetime < mdy("7/1/2018", tz = "US/Central")
+    ) %>%
     mutate(order.day = floor_date(order.datetime, unit = "days")) %>%
     distinct(millennium.id, order.day, .keep_all = TRUE)
 
