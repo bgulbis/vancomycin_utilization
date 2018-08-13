fd1a9b8412d87b0bd0ed61b262fb1082fb9ac621
Brian Gulbis <brian.gulbis@gmail.com>
2018-08-13 15:42:08 -0500

    update vanc consult utilization through 2018-07

--- src/02_service-utilization.R
+++ src/02_service-utilization.R
@@ -1,8 +1,14 @@
 library(tidyverse)
 library(lubridate)
 library(edwr)
+library(openxlsx)
+
+#   * Scheduled Queries/consults_vancomycin
+#       - Date: 7/1/2017 - 5/1/2018
 
 dir_raw <- "data/raw/consults"
+
+dirr::gzip_files(dir_raw)
 
 consults <- read_data(dir_raw, "consults", FALSE) %>%
     rename(millennium.id = `Encounter Identifier`,
@@ -10,8 +16,8 @@ consults <- read_data(dir_raw, "consults", FALSE) %>%
            order = `Mnemonic (Primary Generic) FILTER ON`,
            order.location = `Nurse Unit (Order)`) %>%
     format_dates("order.datetime") %>%
-    filter(order.datetime >= mdy("10/1/2017", tz = "US/Central"),
-           order.datetime < mdy("1/1/2018", tz = "US/Central")) %>%
+    filter(order.datetime >= mdy("7/1/2017", tz = "US/Central"),
+           order.datetime < mdy("8/1/2018", tz = "US/Central")) %>%
     mutate(order.day = floor_date(order.datetime, unit = "days")) %>%
     distinct(millennium.id, order.day, .keep_all = TRUE)
 
@@ -19,3 +25,5 @@ monthly <- consults %>%
     mutate(order.month = floor_date(order.datetime, unit = "month")) %>%
     distinct(millennium.id, order.month) %>%
     count(order.month)
+
+write.xlsx(monthly, "data/external/consults/2018-07.xlsx")
