9eb402ca37e4b51f84c9ae89f52b5e59e2baa6c4
Brian Gulbis <brian.gulbis@gmail.com>
2018-09-06 14:33:11 -0500

    save spreadsheet

--- src/02_service-utilization.R
+++ src/02_service-utilization.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(lubridate)
 library(edwr)
+library(openxlsx)
 
 dir_raw <- "data/raw/consults"
 
@@ -14,7 +15,7 @@ consults <- read_data(dir_raw, "consults", FALSE) %>%
     format_dates("order.datetime") %>%
     filter(
         order.datetime >= mdy("7/1/2017", tz = "US/Central"),
-        order.datetime < mdy("7/1/2018", tz = "US/Central")
+        order.datetime < mdy("9/1/2018", tz = "US/Central")
     ) %>%
     mutate(order.day = floor_date(order.datetime, unit = "days")) %>%
     distinct(millennium.id, order.day, .keep_all = TRUE)
@@ -22,4 +23,14 @@ consults <- read_data(dir_raw, "consults", FALSE) %>%
 monthly <- consults %>%
     mutate(order.month = floor_date(order.datetime, unit = "month")) %>%
     distinct(millennium.id, order.month) %>%
-    count(order.month)
+    count(order.month) %>%
+    mutate_at("order.month", as.Date) %>%
+    rename(
+        month = order.month,
+        num_patients = n
+    )
+
+write.xlsx(
+    monthly,
+    "/home/brian/Public/W_Pharmacy/Dosing Services/vancomycin_utilization.xlsx"
+)
